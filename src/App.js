b1248123c9eee1c954aab687263b0f5dcef9c4c4
Volodymyr <volodymyr.blyntsov@gmail.com>
2022-06-29 14:07:09 +0300

    Posts
    Made a modal window, when the user clicks it, you can add a post (custom title and custom description). Implemented search by added posts (by title). Used "React Transition Group" library for animation of appearance/deletion of posts. Created a custom hook to minimize the code in the App component.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import React, { useRef, useState } from "react";
+import React, { useState, useMemo } from "react";
+import { usePosts } from "./components/hooks/usePosts";
+import PostFilter from "./components/PostFilter";
 import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
+import MyModal from "./components/UI/MyModal/MyModal";
 import MySelect from "./components/UI/select/MySelect";
 import './styles/App.css'
 
@@ -8,48 +13,31 @@ function App(props) {
   const [posts, setPosts] = useState([
     {id: 1, title: 'JavaScript', body: 'A: Description about JS'},
     {id: 2, title: 'Python', body: 'B: Description about Python'},
-    {id: 3, title: 'C++', body: 'C: Description about C++'}
+    {id: 3, title: 'C++', body: 'C: Description about C++'} 
   ])
-
-  const [selectedSort, setSelectedSort] = useState('')
+  const [filter, setFilter] = useState({sort: '', query: ''})
+  const [modal, setModal] = useState(false);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
+    setModal(false)
   }
 
   const removePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id))
   }
 
-  const sortPosts = (sort) => {
-    setSelectedSort(sort);
-    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
-  }
-
   return (
     <div>
-      <PostForm create={createPost} />
-      
-      <hr style={{margin: '15px 0'}} />
-      <div>
-          <MySelect 
-              value={selectedSort}
-              onChange={sortPosts}
-              defaultValue='Сортировка по'
-              options={[
-                  {value: 'title', name: 'По названию'},
-                  {value: 'body', name: 'По описанию'}
-              ]} />
-      </div>
-
-
-      {
-        posts.length !== 0
-        ? <PostList remove={removePost} posts={posts} title='Список постов' />
-        : <h1 style={{textAlign: 'center'}}>
-              Постов не найдено!  
-          </h1>
-      }
+      <MyButton style={{marginTop: '30px'}} onClick={() => setModal(true)}>
+        Создать пользователя
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost} />
+      </MyModal>
+      <PostFilter filter={filter} setFilter={setFilter} />
+      <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Список постов' />
     </div>
   )
 }
