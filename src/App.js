b748d9d38e5e972c69bf9b4414b2f40d2f14e83c
Volodymyr <volodymyr.blyntsov@gmail.com>
2022-06-29 12:22:57 +0300

    Posts
    Sorting has been realizeg (sorting by name, sorting by description). In addition, the "Delete button" now actually deletes posts, not just a static button with no action.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,55 @@
 import React, { useRef, useState } from "react";
-import PostItem from "./components/PostItem";
+import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
-import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
+import MySelect from "./components/UI/select/MySelect";
 import './styles/App.css'
 
 function App(props) {
   const [posts, setPosts] = useState([
-    {id: 1, title: 'JavaScript', body: 'Description'},
-    {id: 2, title: 'JavaScript', body: 'Description'},
-    {id: 3, title: 'JavaScript', body: 'Description'}
+    {id: 1, title: 'JavaScript', body: 'A: Description about JS'},
+    {id: 2, title: 'Python', body: 'B: Description about Python'},
+    {id: 3, title: 'C++', body: 'C: Description about C++'}
   ])
-  const [post, setPost] = useState({
-    title: '',
-    body: ''
-  })
 
-  const addNewPost = (e) => {
-    e.preventDefault()
-    setPosts([...posts, {...post, id: Date.now()}])
-    setPost({title: '', body: ''})
+  const [selectedSort, setSelectedSort] = useState('')
 
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost])
+  }
+
+  const removePost = (post) => {
+    setPosts(posts.filter(p => p.id !== post.id))
+  }
+
+  const sortPosts = (sort) => {
+    setSelectedSort(sort);
+    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
   }
 
   return (
     <div>
-      <form>
-        <MyInput value={post.title} onChange={e => setPost({...post, title: e.target.value})} type="text" placeholder="Название поста" />
-        <MyInput value={post.body} onChange={e => setPost({...post, body: e.target.value})} type="text" placeholder="Описание поста" />
-        <MyButton onClick={addNewPost}> Создать пост </MyButton>
-      </form>
-      <PostList posts={posts} title='Список постов' />
+      <PostForm create={createPost} />
+      
+      <hr style={{margin: '15px 0'}} />
+      <div>
+          <MySelect 
+              value={selectedSort}
+              onChange={sortPosts}
+              defaultValue='Сортировка по'
+              options={[
+                  {value: 'title', name: 'По названию'},
+                  {value: 'body', name: 'По описанию'}
+              ]} />
+      </div>
+
+
+      {
+        posts.length !== 0
+        ? <PostList remove={removePost} posts={posts} title='Список постов' />
+        : <h1 style={{textAlign: 'center'}}>
+              Постов не найдено!  
+          </h1>
+      }
     </div>
   )
 }
